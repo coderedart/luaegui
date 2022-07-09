7b8d018d5b886604044cab23457cfad55abca62d
Red Artist <redartofcode@gmail.com>
2022-07-09 18:55:42 +0530

    fix sample.lua. used wrong variable for open Window

--- examples/sample.lua
+++ examples/sample.lua
@@ -3,21 +3,19 @@ My_plugin.window_options = {
     title = "my lua window",
     open = true,
     ui_function = function(ui)
-
         ui:columns(3, function(columns)
             columns[1]:label("column 1")
             local response, background = columns[1]:color_edit_button_srgba(My_plugin.background)
             My_plugin.background = background
             columns[2]:label("column 2")
             columns[3]:label("column 3")
-
         end)
 
     end,
     window_configuration_function = function(ctx, window)
         window:open(My_plugin.window_options.open)
         local open, window_response, inner_response = window:show(ctx, My_plugin.window_options.ui_function)
-        My_plugin.open = open
+        My_plugin.window_options.open = open
     end
 }
 My_plugin.button_options = {
